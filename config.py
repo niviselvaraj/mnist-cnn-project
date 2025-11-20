88c72527dfbb366ac81cb81197bb7c5eb94f49fb
niviselvaraj <138593053+niviselvaraj@users.noreply.github.com>
2025-11-20 02:36:17 +0000

    change mobilenet_model file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR=os.path.dirname.(os.path.abspath(__file__))
+BASE_DIR=os.path.dirname(os.path.abspath(__file__))
 OUTPUT_DIR=os.path.join(BASE_DIR,"output")
 os.makedirs(OUTPUT_DIR,exist_ok=True)
 NUM_FRAMES=30
